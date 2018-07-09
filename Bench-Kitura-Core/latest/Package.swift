6c1392adf475c2623c28181ad2164bc4fc7ab6eb
David Jones <djones6@uk.ibm.com>
2018-07-09 11:56:06 +0100

    Add simple QueryParams bench

--- Bench-Kitura-Core/latest/Package.swift
+++ Bench-Kitura-Core/latest/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Bench-Kitura-Core",
-            targets: ["HelloWorld", "HelloMiddleware", "HelloLogging", "HelloSSL", "JSON", "StaticFile", "ThinkTime", "Codable", "CodableLarge", "CodableSmall", "CodableRouting"]),
+            targets: ["HelloWorld", "QueryParams", "HelloMiddleware", "HelloLogging", "HelloSSL", "JSON", "StaticFile", "ThinkTime", "Codable", "CodableLarge", "CodableSmall", "CodableRouting"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -28,6 +28,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "HelloWorld",   dependencies: benchmarkDependencies),
+        .target(name: "QueryParams",   dependencies: benchmarkDependencies),
         .target(name: "HelloLogging", dependencies: benchmarkDependencies),
         .target(name: "HelloSSL",     dependencies: benchmarkDependencies),
         .target(name: "JSON",         dependencies: benchmarkDependencies),
